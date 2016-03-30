e2358904d8209827d0e94fecfc141fb158bbe072
Miloš Kočan <milos.kocan@akademiasovy.sk>
2016-03-30 14:22:14 +0200

    V1 IT Test
    V1 IT test prvy komit

---
+++ IT - Test/IT - Test/Form1.Designer.cs
@@ -0,0 +1,403 @@
+﻿namespace IT___Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl1OK = new System.Windows.Forms.Label();
+            this.lbl2OK = new System.Windows.Forms.Label();
+            this.lbl3OK = new System.Windows.Forms.Label();
+            this.lbl4OK = new System.Windows.Forms.Label();
+            this.lbl5OK = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lbl6OK = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3OK = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(29, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Prva PC Siet sa vola?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(241, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 101);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "2. Ktory protokol pracuje na  transortnej vrstve ?";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(316, 43);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(78, 28);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "HTTP";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(185, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(44, 28);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "IP";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 43);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(65, 28);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TCP";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(20, 37);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 28);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "132.168.1.20";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(174, 37);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(134, 28);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "168.259.1.99";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(340, 37);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(109, 28);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "255.256.0";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(469, 37);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(134, 28);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "168.187.1.99";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox2.Location = new System.Drawing.Point(23, 273);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(471, 101);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "4. Na ktorej vrstve pracuje Switch?";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(316, 43);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(89, 28);
+            this.radioButton4.TabIndex = 2;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Sietova";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(185, 43);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(92, 28);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Linkova";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(20, 43);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(91, 28);
+            this.radioButton6.TabIndex = 0;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Fyzicka";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(29, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(347, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "5. Najpouzivanejsia sietova topologia je?";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kruhova",
+            "Hviezdicova",
+            "Konektorova",
+            "Spajacia"});
+            this.comboBox1.Location = new System.Drawing.Point(391, 398);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "Vyber moznost";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lbl1OK
+            // 
+            this.lbl1OK.AutoSize = true;
+            this.lbl1OK.Location = new System.Drawing.Point(360, 36);
+            this.lbl1OK.Name = "lbl1OK";
+            this.lbl1OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl1OK.TabIndex = 12;
+            this.lbl1OK.Text = "EROR";
+            // 
+            // lbl2OK
+            // 
+            this.lbl2OK.AutoSize = true;
+            this.lbl2OK.Location = new System.Drawing.Point(534, 124);
+            this.lbl2OK.Name = "lbl2OK";
+            this.lbl2OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl2OK.TabIndex = 13;
+            this.lbl2OK.Text = "EROR";
+            // 
+            // lbl3OK
+            // 
+            this.lbl3OK.AutoSize = true;
+            this.lbl3OK.Location = new System.Drawing.Point(534, 232);
+            this.lbl3OK.Name = "lbl3OK";
+            this.lbl3OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl3OK.TabIndex = 14;
+            this.lbl3OK.Text = "EROR";
+            // 
+            // lbl4OK
+            // 
+            this.lbl4OK.AutoSize = true;
+            this.lbl4OK.Location = new System.Drawing.Point(534, 325);
+            this.lbl4OK.Name = "lbl4OK";
+            this.lbl4OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl4OK.TabIndex = 15;
+            this.lbl4OK.Text = "EROR";
+            // 
+            // lbl5OK
+            // 
+            this.lbl5OK.AutoSize = true;
+            this.lbl5OK.Location = new System.Drawing.Point(534, 403);
+            this.lbl5OK.Name = "lbl5OK";
+            this.lbl5OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl5OK.TabIndex = 16;
+            this.lbl5OK.Text = "EROR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(29, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(349, 24);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "6. Od akeho datumu sa datuje cas v PC?";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(391, 449);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // lbl6OK
+            // 
+            this.lbl6OK.AutoSize = true;
+            this.lbl6OK.Location = new System.Drawing.Point(615, 453);
+            this.lbl6OK.Name = "lbl6OK";
+            this.lbl6OK.Size = new System.Drawing.Size(38, 13);
+            this.lbl6OK.TabIndex = 19;
+            this.lbl6OK.Text = "EROR";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox3.Location = new System.Drawing.Point(23, 180);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(609, 78);
+            this.groupBox3.TabIndex = 20;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "4. Ktora IP Adresa je spravna?";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // label3OK
+            // 
+            this.label3OK.AutoSize = true;
+            this.label3OK.Location = new System.Drawing.Point(650, 217);
+            this.label3OK.Name = "label3OK";
+            this.label3OK.Size = new System.Drawing.Size(38, 13);
+            this.label3OK.TabIndex = 21;
+            this.label3OK.Text = "EROR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 514);
+            this.Controls.Add(this.label3OK);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.lbl6OK);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl5OK);
+            this.Controls.Add(this.lbl4OK);
+            this.Controls.Add(this.lbl3OK);
+            this.Controls.Add(this.lbl2OK);
+            this.Controls.Add(this.lbl1OK);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "IT Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl1OK;
+        private System.Windows.Forms.Label lbl2OK;
+        private System.Windows.Forms.Label lbl3OK;
+        private System.Windows.Forms.Label lbl4OK;
+        private System.Windows.Forms.Label lbl5OK;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lbl6OK;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3OK;
+    }
+}
+
---
+++ IT - Test/IT - Test/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT___Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { String str1 = "arpanet";
+            if(textBox1.Text.ToLower().Trim() == str1)
+            {
+                lbl1OK.Text = "OK";
+            }
+
+            else
+            {
+                lbl1OK.Text = "EROR";
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {    
+
+            if(radioButton1.Checked == false)
+            {
+                lbl2OK.Text = "OK";
+            }
+
+            else
+            {
+                lbl2OK.Text = "EROR";
+            }
+        }
+       
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false && checkBox4.Checked == false)
+            {
+                label3OK.Text = "OK";
+            }
+
+            else
+            {
+                label3OK.Text = "CHYBA";
+            }
+           
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            if (radioButton5.Checked == false)
+            {
+                lbl4OK.Text = "OK";
+            }
+
+            else
+            {
+                lbl4OK.Text = "EROR";
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem == "Hviezdicova")
+            {
+                lbl5OK.Text = "OK";
+
+            }
+
+            else
+            {
+                lbl5OK.Text = "EROR";
+            }
+        }
+
+        
+        public void kontrolaDatumu()
+        {
+            string datum = dateTimePicker1.Value.ToShortDateString();
+
+            if (datum == "01.01.1970")
+            {
+                lbl6OK.Text = "OK";
+            }
+
+            else {
+                lbl6OK.Text = "OK";
+            }
+        }
+    }
+}
